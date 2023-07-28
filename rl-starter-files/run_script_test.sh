dc038ab5c087e443759fee88fc2c97ffe19ef1fc
CorinaCaraconcea <cori.caraconcea@gmail.com>
2023-07-28 11:13:52 +0100

    task_id fix

--- rl-starter-files/run_script_test.sh
+++ rl-starter-files/run_script_test.sh
@@ -47,10 +47,10 @@ number=$SGE_TASK_ID
 env ="`sed -n ${SGE_TASK_ID}'{p;q}' $paramfile | awk '{print $1}'`"
 folder_name="`sed -n ${SGE_TASK_ID}'{p;q}' $paramfile | awk '{print $2}'`"
 frames="`sed -n ${SGE_TASK_ID}'{p;q}' $paramfile | awk '{print $3}'`"
-intrinsic_reward_model="`sed -n ${SGE_TASK_ID}p $paramfile | awk '{print $4}'`"
-beta_coeff="`sed -n ${SGE_TASK_ID}p $paramfile | awk '{print $5}'`"
-no_skills="`sed -n ${SGE_TASK_ID}p $paramfile | awk '{print $6}'`"
-window_size="`sed -n ${SGE_TASK_ID}p $paramfile | awk '{print $7}'`"
+intrinsic_reward_model="`sed -n ${SGE_TASK_ID}'{p;q}' $paramfile | awk '{print $4}'`"
+beta_coeff="`sed -n ${SGE_TASK_ID}'{p;q}' $paramfile | awk '{print $5}'`"
+no_skills="`sed -n ${SGE_TASK_ID}'{p;q}' $paramfile | awk '{print $6}'`"
+window_size="`sed -n ${SGE_TASK_ID}'{p;q}' $paramfile | awk '{print $7}'`"
 
 
 echo "$env" "$folder_name" "$frames" "$intrinsic_reward_model" "$beta_coeff" "$no_skills" "$window_size"
