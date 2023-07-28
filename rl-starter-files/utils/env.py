42d165e0c3a307fb5c1cc1a5dd63baccd6a27dea
CorinaCaraconcea <cori.caraconcea@gmail.com>
2023-07-28 11:27:22 +0100

    fix

--- rl-starter-files/utils/env.py
+++ rl-starter-files/utils/env.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 
 
 def make_env(env_key, seed=None, render_mode=None):
-    print("env_key")
+    print(env_key)
     env = gym.make(env_key, render_mode=render_mode)
     env.reset(seed=seed)
     return env
