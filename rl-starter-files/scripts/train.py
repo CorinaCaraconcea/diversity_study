4cf29e0387481b680a571c871593149003787009
CorinaCaraconcea <cori.caraconcea@gmail.com>
2023-07-28 10:39:23 +0100

    update parameters file

--- rl-starter-files/scripts/train.py
+++ rl-starter-files/scripts/train.py
@@ -216,7 +216,7 @@ if __name__ == "__main__":
                                 args.entropy_coef, args.value_loss_coef, args.max_grad_norm, args.recurrence,
                                 args.optim_eps, args.clip_eps, args.epochs, args.batch_size, preprocess_obss, args.intrinsic_coef,args.number_skills,args.window_size)
         elif args.intrinsic_reward_model == "TrajectoryWindowCount":
-            algo = ppo.PPOAlgo(envs,obs_space, action_space, acmodel,state_action_model, args.intrinsic_reward_model, device, args.frames_per_proc, args.discount, args.lr, args.gae_lambda,
+            algo = ppo.PPOAlgo(envs,obs_space, action_space, acmodel,None, args.intrinsic_reward_model, device, args.frames_per_proc, args.discount, args.lr, args.gae_lambda,
                                 args.entropy_coef, args.value_loss_coef, args.max_grad_norm, args.recurrence,
                                 args.optim_eps, args.clip_eps, args.epochs, args.batch_size, preprocess_obss, args.intrinsic_coef,args.number_skills,args.window_size)
         elif args.intrinsic_reward_model == "DIAYN":
@@ -254,6 +254,10 @@ if __name__ == "__main__":
     # Track hyperparameters and run metadata
     config={
         "model": model_flag,
+        "seed": args.seed,
+        "intr_coeff": args.intrinsic_coef,
+        "window_size": args.window_size,
+        "no_skills": args.number_skills,
         "performance metric": "extrinsic reward"    
         })
 
